297e95a03ca2b5ab56d5c6855db5ff46cf1544ab
ConsoleCap45 <tjagan42@gmail.com>
2024-04-28 10:10:24 +0530

    firebase update

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,19 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCIQl0weHWkyX7FNmGNCTElUYL9aL0HPMA",
-    authDomain: "online-book-store-78516.firebaseapp.com",
-    projectId: "online-book-store-78516",
-    storageBucket: "online-book-store-78516.appspot.com",
-    messagingSenderId: "282181520275",
-    appId: "1:282181520275:web:0ce28538cea0433ea83666"
-  };
+  apiKey: "AIzaSyAqIEsm8CUEqtoiQ0k4CzSvt5_7zV6xPvU",
+  authDomain: "book-inventory-b75fe.firebaseapp.com",
+  projectId: "book-inventory-b75fe",
+  storageBucket: "book-inventory-b75fe.appspot.com",
+  messagingSenderId: "800257054628",
+  appId: "1:800257054628:web:99c629e2d02729b7825331",
+  measurementId: "G-9V7VV2Y6RH"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export default app;
+const analytics = getAnalytics(app);
